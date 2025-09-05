98811f6d7cdd48a99a172598031b2bf55e6c990a
ritik kumar <kr.ritik1510@gmail.com>
2025-09-05 21:14:23 +0530

    add tooltip for mouse hover effect

--- React_Amazon_frontend/src/components/ShopSection.tsx
+++ React_Amazon_frontend/src/components/ShopSection.tsx
@@ -1,8 +1,10 @@
 import { cardsData } from "@/data/appData";
+import Tooltip from "./ToolTip";
 
 function ShopSection() {
   return (
     <div className="shop-section">
+      <div className="hero-section"></div>
       {cardsData.map((card, index) => (
         <div key={index} className={`box-${(index % 4) + 1} box`}>
           <div className="box-content">
@@ -11,9 +13,11 @@ function ShopSection() {
               className="box-image"
               style={{ backgroundImage: `url(${card.image})` }}
             ></div>
-            <div className="box-btn-text">
-              <a href={card.link}>{card.linkText}</a>
-            </div>
+            <Tooltip message="Sorry, it's just a frontend image">
+              <div className="box-btn-text">
+                <a href={card.link}>{card.linkText}</a>
+              </div>
+            </Tooltip>
           </div>
         </div>
       ))}
